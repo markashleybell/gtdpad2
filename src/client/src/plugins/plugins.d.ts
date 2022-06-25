cb924b55f0517db511ea5071f8fc303fef096622
Mark Bell <me@markb.co.uk>
2022-06-25 10:37:34 +0100

    Delete client project (broken anyway, and just causing a ton of security alerts)

--- src/client/src/plugins/plugins.d.ts
+++
@@ -1,9 +0,0 @@
-import { IHttpClient } from './IHttpClient';
-
-declare module '@vue/runtime-core' {
-    export interface ComponentCustomProperties {
-        $api: IHttpClient;
-        $route: RouteLocationNormalizedLoaded;
-        $router: Router;
-    }
-}
